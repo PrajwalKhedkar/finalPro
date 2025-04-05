d273674a0d73f16da32a04bd13b7ebb5a56982c3
PrajwalKhedkar <prkhedkar12150@gmail.com>
2025-04-05 21:51:51 +0530

    by Tony

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 const userRouter = require(`./routes/user_router`);
 const productRouter = require(`./routes/product_router`);
+const ocr=require(`./routes/ocr`);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(`/users`, userRouter);
 app.use(`/products`,productRouter);
-
+app.use(`/ocr`,ocr)
 app.get("/", (req, res) => {
     res.send("Server is running...");
   });
